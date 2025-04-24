b396d838c6f1a72dbeb3a1c705403f95c8302179
ochir <ochir.b@tanasoft.mn>
2025-04-24 19:07:10 +0800

    update

---
+++ stack/car_fleet.go
@@ -0,0 +1,9 @@
+package stack
+
+func CarFleet(target int, position []int, speed []int) int {
+	res := 0
+	slowest := len(position) - 1
+	for i := len(position) - 1; i > 0; i-- {
+	}
+	return res
+}
--- stack/daily_temperatures.go
+++ stack/daily_temperatures.go
@@ -1,12 +1,15 @@
 package stack
 
 func DailyTemperatures(temperatures []int) []int {
-	res := []int{}
-	stack := []map[int]int{}
+	res := make([]int, len(temperatures))
+	stack := []int{}
 	for i, t := range temperatures {
-		for stack[len(stack)-1] {
-			temp, indx := stack[len(stack)-1]
+		for len(stack) > 0 && t > temperatures[stack[len(stack)-1]] {
+			stackInd := stack[len(stack)-1]
+			res[stackInd] = i - stackInd
+			stack = stack[:len(stack)-1]
 		}
+		stack = append(stack, i)
 	}
 	return res
 }
